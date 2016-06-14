d9c8cb27c9ef6317ff3389715eed58ec64cf0f4f
cale <cdqwertz@gmail.com>
2016-06-14 17:56:03 +0200

    added vec2.abs + vec2.dist + vec2.length

--- src/vector.js
+++ src/vector.js
@@ -24,4 +24,19 @@ function vec2(x, y) {
 		this.y /= other.y;
 		return this;
 	};
+
+	this.dist = function(other) {
+		return math.pow(math.pow(this.x-other.x)+math.pow(this.y-other.y),0.5);
+	};
+
+	this.length = function() {
+		return math.pow(math.pow(this.x)+math.pow(this.y),0.5);
+	};
+
+	this.abs = function() {
+		this.x = math.abs(this.x);
+		this.y = math.abs(this.y);
+		return this;
+	};
+
 }
