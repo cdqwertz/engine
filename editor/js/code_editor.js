ecce2453e592ff441883c180b37de221151f5432
cale <cdqwertz@gmail.com>
2016-06-21 18:44:16 +0200

    code editor: added support for multiple inputs and outputs

--- editor/js/code_editor.js
+++ editor/js/code_editor.js
@@ -18,15 +18,18 @@ var code_editor = new function() {
 				   x < this.components[this.component][i][0]+100 &&
 				   y > this.components[this.component][i][1] &&
 				   y < this.components[this.component][i][1]+100) {
-					this.SelectedObject = [0,i];
+					this.SelectedObject = [0,i,0];
 					this.updateEditorGUI();
 				}
-				if(x > this.components[this.component][i][0]+100 &&
-				   x < this.components[this.component][i][0]+115 &&
-				   y > this.components[this.component][i][1]+10 &&
-				   y < this.components[this.component][i][1]+25) {
-					this.SelectedObject = [1,i];
-					this.updateEditorGUI();
+				var outputs = this.Cmds[this.findCmd(this.components[this.component][i][2])][2];
+				for(var j = 0; j < outputs; j++){
+					if(x > this.components[this.component][i][0]+100 &&
+					   x < this.components[this.component][i][0]+115 &&
+					   y > this.components[this.component][i][1]+10+j*20 &&
+					   y < this.components[this.component][i][1]+25+j*20) {
+						this.SelectedObject = [1,i,j];
+						this.updateEditorGUI();
+					}
 				}
 			}
 		}
@@ -38,12 +41,15 @@ var code_editor = new function() {
 		var y = e.clientY;
 		if(this.component != -1 && this.SelectedObject[0] == 1) {
 			for(var i = 0; i < this.components[this.component].length; i++){
-				if(x > this.components[this.component][i][0]-15 &&
-				   x < this.components[this.component][i][0]+10 &&
-				   y > this.components[this.component][i][1] &&
-				   y < this.components[this.component][i][1]+25) {
-					this.components[this.component][this.SelectedObject[1]][3].push([i]);
-					this.updateEditorGUI();
+				var inputs = this.Cmds[this.findCmd(this.components[this.component][i][2])][1];
+				for(var j = 0; j < inputs; j++){
+					if(x > this.components[this.component][i][0]-15 &&
+					   x < this.components[this.component][i][0]+10 &&
+					   y > this.components[this.component][i][1]+20*j &&
+					   y < this.components[this.component][i][1]+25+20*j) {
+						this.components[this.component][this.SelectedObject[1]][3].push([i, j, this.SelectedObject[2]]);
+						this.updateEditorGUI();
+					}
 				}
 			}
 		}
@@ -80,17 +86,23 @@ var code_editor = new function() {
 		codeCtx.fillText(d[2],d[0]+20,d[1] + 20);
 
 		//draw inputs
-		codeCtx.strokeRect(d[0]-15,d[1]+10, 15, 15);
+		var inputs = this.Cmds[this.findCmd(d[2])][1]
+		for(var i = 0; i < inputs; i++){
+			codeCtx.strokeRect(d[0]-15,d[1]+10+i*20, 15, 15);
+		}
 		
 		//draw outputs
-		codeCtx.strokeRect(d[0]+100,d[1]+10, 15, 15);
+		var outputs = this.Cmds[this.findCmd(d[2])][2]
+		for(var i = 0; i < outputs; i++){
+			codeCtx.strokeRect(d[0]+100,d[1]+10+i*20, 15, 15);
+		}
 
 		//draw connections
 		for(var i = 0; i < d[3].length; i++){
 			codeCtx.beginPath();
-			codeCtx.moveTo(d[0]+107,d[1]+17, 15, 15);
+			codeCtx.moveTo(d[0]+107,d[1]+17+20*d[3][i][2], 15, 15);
 			var c = this.components[this.component][d[3][i][0]];
-			codeCtx.lineTo(c[0]-10,c[1]+17, 15, 15);
+			codeCtx.lineTo(c[0]-10,c[1]+17+20*d[3][i][1], 15, 15);
 			codeCtx.stroke();
 		}
 	}
@@ -105,11 +117,20 @@ var code_editor = new function() {
 		this.updateEditorGUI();
 	}
 
-	this.RegisterCmd = function(name) {
-		this.Cmds.push([name])
+	this.RegisterCmd = function(name, inputs, outputs) {
+		this.Cmds.push([name, inputs, outputs])
 	}
+
+	this.findCmd = function(name) {
+		for(var i = 0; i < this.Cmds.length; i++){
+			if(this.Cmds[i][0] == name) {
+				return i;
+			}
+		}
+		return -1;
+	};
 
 	//register commands
 
-	this.RegisterCmd("test");
+	this.RegisterCmd("test", 2, 3);
 }();
